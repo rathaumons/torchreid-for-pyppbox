10d4d10407940849705600dc65a08e75d29909b9
rathaumons <60795657+rathaumons@users.noreply.github.com>
2023-05-12 01:14:09 +0200

    Update build_creat_whl.cmd

--- build_creat_whl.cmd
+++ build_creat_whl.cmd
@@ -3,6 +3,7 @@ setlocal
 cd /d %~dp0
 set "PYTHONWARNINGS=ignore"
 python -m pip install --upgrade pip
+pip install wheel
 pip install build
 call pyppbox_torchreid\metrics\rank_cylib\build.cmd
 call pyppbox_torchreid\metrics\rank_cylib\test_cython.cmd
